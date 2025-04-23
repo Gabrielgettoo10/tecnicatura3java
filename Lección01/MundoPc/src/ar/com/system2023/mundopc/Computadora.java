659e74d8786330281e9d95a41e44196700983000
Gabriel Garino <gabrielgarino10@gmail.com>
2025-04-23 16:21:22 -0300

    Subiendo una parte de la clase 1 de java

---
+++ Lección01/MundoPc/src/ar/com/system2023/mundopc/Computadora.java
@@ -0,0 +1,16 @@
+
+package ar.com.system2023.mundopc;
+
+public class Computadora {
+    private final int idComputadora;
+    private String nombre;
+    private Monitor monitor;
+    private Teclado teclado;
+    private Raton raton;
+    private static int contadorComputadoras;
+    
+    
+//Constructor vacio
+    private Computadora(){
+        this.idComputadora = ++
+    }
