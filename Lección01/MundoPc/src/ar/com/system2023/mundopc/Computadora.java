fe23b3349ba9a1a66b129449882f7201c79c5d50
Gabriel <gabrielgarino10@gmail.com>
2025-04-24 17:15:39 -0300

    Clase 1 Laboratorio en Java completado

--- Lección01/MundoPc/src/ar/com/system2023/mundopc/Computadora.java
+++ Lección01/MundoPc/src/ar/com/system2023/mundopc/Computadora.java
@@ -12,5 +12,57 @@ public class Computadora {
     
 //Constructor vacio
     private Computadora(){
-        this.idComputadora = ++
+        this.idComputadora = ++Computadora.contadorComputadoras;
     }
+
+//Constructor 2
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.raton = raton;
+        this.teclado = teclado;
+    }
+    
+    public int getIdComputadora(){
+        return idComputadora;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Teclado getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    public Raton getRaton() {
+        return raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", teclado=" + teclado + ", raton=" + raton + '}';
+    }
+     
+    
+}
